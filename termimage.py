7f21d218bf0e5e2daba88fe2386e9d2028a2e2d5
jaegr <karlre@gmail.com>
2012-04-28 23:43:31 +0200

    Moved color conversions to a separate file

--- termimage.py
+++ termimage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import rgb_values
+import color_conversions
 import Image, ImageOps, ImageEnhance
 import urllib2
 from cStringIO import StringIO
@@ -42,71 +43,6 @@ parser.add_option('--bw', action='store_true', dest='black_and_white', default=F
 
 (options, args) = parser.parse_args()
 
-
-def rgb_to_xyz(R, G, B):
-    '''From http://www.easyrgb.com/index.php?X=MATH'''
-    var_R = (R / 255.)
-    var_G = (G / 255.)
-    var_B = (B / 255.)
-
-    if var_R > 0.04045:
-        var_R = ((var_R + 0.055) / 1.055) ** 2.4
-    else:
-        var_R /= 12.92
-
-    if var_G > 0.04045:
-        var_G = (( var_G + 0.055) / 1.055) ** 2.4
-    else:
-        var_G /= 12.92
-    if var_B > 0.04045:
-        var_B = ((var_B + 0.055) / 1.055) ** 2.4
-    else:
-        var_B /= 12.92
-
-    var_R *= 100
-    var_G *= 100
-    var_B *= 100
-
-    X = var_R * 0.4124 + var_G * 0.3576 + var_B * 0.1805
-    Y = var_R * 0.2126 + var_G * 0.7152 + var_B * 0.0722
-    Z = var_R * 0.0193 + var_G * 0.1192 + var_B * 0.9505
-
-    return X,Y,Z
-
-def xyz_to_cielab(X, Y, Z):
-    '''From http://www.easyrgb.com/index.php?X=MATH'''
-    ref_X =  95.047
-    ref_Y = 100.000
-    ref_Z = 108.883
-
-    var_X = X / ref_X
-    var_Y = Y / ref_Y
-    var_Z = Z / ref_Z
-
-    if var_X > 0.008856:
-        var_X **= (1./3.)
-    else:
-        var_X = (7.787 * var_X) + (16. / 116.)
-    if var_Y > 0.008856:
-        var_Y **= (1./3.)
-    else:
-        var_Y = (7.787 * var_Y) + (16. / 116.)
-    if var_Z > 0.008856:
-        var_Z **= ( 1./3. )
-    else:
-        var_Z = (7.787 * var_Z) + (16. / 116.)
-
-    CIE_L = (116 * var_Y) - 16.
-    CIE_a = 500. * (var_X - var_Y)
-    CIE_b = 200. * (var_Y - var_Z)
-
-    return CIE_L, CIE_a, CIE_b
-
-def rgb_to_cielab(R, G, B):
-    X, Y, Z = rgb_to_xyz(R, G ,B) 
-    CIE_L, CIE_a, CIE_b = xyz_to_cielab(X, Y, Z)
-    return CIE_L, CIE_a, CIE_b
-
 if options.irc:
     rgb_values = rgb_values.irc_rgb_values
 elif options.high_res and not options.xterm:
@@ -119,7 +55,7 @@ elif options.xterm:
 else:
     rgb_values = rgb_values.default_rgb_values
 
-lab_values = [rgb_to_cielab(r,g,b) for (r,g,b) in rgb_values]
+lab_values = [color_conversions.rgb_to_cielab(r,g,b) for (r,g,b) in rgb_values]
 
 index_to_ansi_front = ['30',
                  '31',
@@ -271,7 +207,7 @@ def get_nearest_rgb(im, x, y, back=False): #deprecated
         r1, g1, b1 = im.getpixel((x, y))
     except IndexError:
         r1 = g1 = b1 = 0
-    l1, a1, b1 = rgb_to_cielab(r1,g1,b1)
+    l1, a1, b1 = color_conversions.rgb_to_cielab(r1,g1,b1)
     for index, (l2, a2, b2) in enumerate(lab_values):
 #        current = sqrt(((r2-r1)*0.3)**2 + ((g2-g1)*0.59)**2 + ((b2-b1)*0.11)**2)
 #        current = sqrt((r2-r1)**2 + (g2-g1)**2 + (b2-b1)**2)
